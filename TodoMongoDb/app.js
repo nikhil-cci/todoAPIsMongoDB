e46fd9581e1f871d0c23ece618a769afefa4bea3
Nikhil Gaonkar <nikhil.gaonkar@creativecapsule.com>
2018-11-08 08:32:12 +0530

    Added body-parser and morgan

--- TodoMongoDb/app.js
+++ TodoMongoDb/app.js
@@ -10,5 +10,13 @@ const express = require('express'),
 app = express();
 var port = process.env.PORT || 3001
 
+//Initialise Mongoose
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
+
+//Add body parser
+app.use(bodyParser.urlencoded({ urlencoded: true }));
+app.use(bodyParser.json());
+
+//Add morgan for logs
+app.use(morgan('short'))
